90512c3ef8447625b4a5e3a1d4b0e6c61e6d70f8
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-13 20:39:56 -0500

    Added the 5 fields and enum to Class 1

--- src/main/java/org/example/Adress.java
+++ src/main/java/org/example/Adress.java
@@ -1,6 +1,14 @@
-package org.example;
+    package org.example;
 
-public class Adress
-{
-    this is a pratcie
+public class Address{
+    // Fields
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+
+    public enum Province {
+        QC, ON, AB, BC, MB, NB, NL, NT, NU, PE, SK, YT
+    }
 }
