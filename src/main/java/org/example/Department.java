acd6f3e0a7d061f728276059b33ce4b932b37f3c
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-14 15:51:13 -0500

    added method to check if name is valid

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -5,4 +5,25 @@ public class Department {
     private String departmentId;
     private String departmeentName;
     private static int nextId = 1;
+
+    //Checker Method
+    /**
+     * Check if the department name is valid
+     * Valid: Only letters and spaces
+     * @param departmentName the department name in question
+     * @return 
+     */
+    public static boolean isDepartmentNameValid(String departmentName) {
+        if (departmentName == null) {
+            return false;
+        }
+        //For Loop
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            
+            if(Character.isLetter(c) == false && c != ' ') {
+                return false;
+            }
+        }
+        return true;
 }
