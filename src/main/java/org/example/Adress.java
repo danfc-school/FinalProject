62d9b689b2b6dc2664185599e6818b15e9fce924
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-14 14:01:21 -0500

    added comments to the methods

--- src/main/java/org/example/Adress.java
+++ src/main/java/org/example/Adress.java
@@ -3,7 +3,7 @@ package org.example;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.EqualsAndHashCode
+import lombok.EqualsAndHashCode;
 
 @Getter
 @Setter
@@ -22,6 +22,12 @@ public class Address{
         QC, ON, AB, BC, MB, NB, NL, NT, NU, PE, SK, YT
     }
     //Helper Method to check if the postcode is valid
+    /**
+     * Checks if the postal code is valid
+     * A valid postal code: Isnt null, must be 6 digits, must be CDCDCD
+     * @param postalCode the postal code in question
+     * @return true if its a valid postal code, false if it isnt
+     */
     public static boolean isPostalCodeValid(String postalCode) {
         if (postalCode == null || postalCode.length() !=6 ) {
             return false;
@@ -43,6 +49,15 @@ public class Address{
         return true;
     }
     //Constructor
+    /**
+     * This creates the Address
+     * If postal code isnt valid, the fields are null
+     * @param streetNo street number
+     * @param street street name
+     * @param city city name
+     * @param province Canadian province
+     * @param postalCode postal code
+     */
     public Address(int streetNo, String street, String city, Province province, String postalCode) {
         if(isPostalCodeValid(postalCode)) {
             this.streetNo = streetNo;
@@ -94,11 +109,20 @@ public class Address{
         }
     }
     //toString Method
+    /**
+     * returns the entire address with all fields
+     * @return the address
+     */
     @Override
     public String toString() {
         return streetNo + " " + street + ", " + city + " " + province + " " + postalCode;
     }
     //Equals Method
+    /**
+     *
+     * @param obj   the reference object with which to compare.
+     * @return true if the obj shows the same address, false if it doesnt
+     */
     @Override
     public boolean equals (Object obj) {
         if (this == obj)
