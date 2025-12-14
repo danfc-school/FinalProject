cd3315e7fdbe21d09618c263fe0c4e5b003a417a
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-13 21:18:37 -0500

    added helper method that checks if the postcode is valid

--- src/main/java/org/example/Adress.java
+++ src/main/java/org/example/Adress.java
@@ -1,4 +1,4 @@
-    package org.example;
+package org.example;
 
 public class Address{
     // Fields
@@ -10,5 +10,26 @@ public class Address{
 
     public enum Province {
         QC, ON, AB, BC, MB, NB, NL, NT, NU, PE, SK, YT
+    }
+    //Helper Method to check if the postcode is valid
+    public static boolean isPostalCodeValid(String postalCode) {
+        if (postalCode == null || postalCode.length() !=6 ) {
+            return false;
+        }
+        //For Loop
+        for (int i = 0; i < 6; i++) {
+            char c = postalCode.charAt(i);
+            if (i % 2 == 0) {
+                if (!Character.isLetter(c)) {
+                    return false;
+                }
+            }
+            else {
+                if (!Character.isDigit(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
