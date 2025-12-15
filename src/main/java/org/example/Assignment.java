1957d6c2465151d053e431cb2ab576b0597d3e27
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-14 20:49:37 -0500

    added fields

---
+++ src/main/java/org/example/Assignment.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class Assignment {
+    //Fields
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+    private static int nextId = 1;
+}
