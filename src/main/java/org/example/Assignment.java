5bf3e3b465cf9345e40526fc8e5d79d7aafd7ad3
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-14 20:55:24 -0500

    added constructor

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -9,4 +9,21 @@ public class Assignment {
     private double weight;
     private ArrayList<Integer> scores;
     private static int nextId = 1;
+
+    //Constructor
+
+    /**
+     * Creates assignment with the name & weight
+     * @param assignmentName assignment name
+     * @param weight assignment weight
+     */
+    public Assignment(String assignmentName, double weight) {
+        this.assignmentId = "A" + nextId;
+        nextId++;
+        
+        this.assignmentName = assignmentName;
+        this.weight = weight;
+        this.scores = new ArrayList<>;
+    }
 }
+
