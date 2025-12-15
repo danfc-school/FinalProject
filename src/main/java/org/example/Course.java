a2aa0a5d3949158a65fc4f94af95219006c22931
danfc-school <2579083@edu.vaniercollege.qc.ca>
2025-12-14 22:10:13 -0500

    added constructor and helper method

--- src/main/java/org/example/Course.java
+++ src/main/java/org/example/Course.java
@@ -2,13 +2,30 @@ package org.example;
 
 import java.util.ArrayList;
 
-public class Course {
-    private String courseId;
-    private String courseName;
-    private double credits;
-    private Department department;
-    private ArrayList<Assignment> assignments;
-    private ArrayList<Student> registeredStudents;
-    private static int nextId = 1;
-    
+    //Fields
+    public class Course {
+        private String courseId;
+        private String courseName;
+        private double credits;
+        private Department department;
+        private ArrayList<Assignment> assignments;
+        private ArrayList<Student> registeredStudents;
+        private static int nextId = 1;
+
+    //Constructor
+    public Course(String courseName, double credits, Department department) {
+        this.courseName = courseName;
+        this.credits = credits;
+        this.department = department;
+
+        this.assignments = new ArrayList<>();
+        this.registeredStudents = new ArrayList<>();
+        this.courseId = generateCourseId();
+    }
+
+    //Helper Method
+    private String generateCourseId() {
+        String id = String.format("C-%s-%02d", department.getDepartmentId(), nextId);
+        return id;
+    }
 }
